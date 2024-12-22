f778d2e9557a1a9d1885f8752a438f46ec459e92
we-z <yousefsalem00@gmail.com>
2024-12-21 23:42:58 -0800

    Initial Commit

---
+++ SF Urbex/SF_UrbexApp.swift
@@ -0,0 +1,17 @@
+//
+//  SF_UrbexApp.swift
+//  SF Urbex
+//
+//  Created by Wheezy Capowdis on 12/21/24.
+//
+
+import SwiftUI
+
+@main
+struct SF_UrbexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SF UrbexTests/SF_UrbexTests.swift
@@ -0,0 +1,17 @@
+//
+//  SF_UrbexTests.swift
+//  SF UrbexTests
+//
+//  Created by Wheezy Capowdis on 12/21/24.
+//
+
+import Testing
+@testable import SF_Urbex
+
+struct SF_UrbexTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
