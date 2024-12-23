30a93b3b346d3c295abc5055a49723ac5f9f3e60
we-z <yousefsalem00@gmail.com>
2024-12-22 19:17:38 -0800

    remove comments

--- SF Urbex/ContentView.swift
+++ SF Urbex/ContentView.swift
@@ -9,43 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     @StateObject private var cloudKitManager = CloudKitManager()
-//    init() {
-//        UITabBar.appearance().backgroundColor = UIColor.systemGray6
-//    }
-    
+
     var body: some View {
         TabView {
             // Home Tab
             VideoFeedView()
                 .tabItem {
                     Image(systemName: "house")
-//                    Text("Home")
                 }
 
             // Upload Tab
             UploadVideoView(cloudKitManager: cloudKitManager)
                 .tabItem {
                     Image(systemName: "plus.app")
-//                    Text("Upload")
                 }
 
             // Profile Tab
             ProfileView()
                 .tabItem {
                     Image(systemName: "person")
-//                    Text("Profile")
                 }
         }
         .accentColor(.primary)
-//        .onAppear {
-//            let appearance = UITabBarAppearance()
-//            appearance.backgroundEffect = UIBlurEffect(style: .systemUltraThinMaterial)
-//            
-//            // Use this appearance when scrolling behind the TabView:
-//            UITabBar.appearance().standardAppearance = appearance
-//            // Use this appearance when scrolled all the way up:
-//            UITabBar.appearance().scrollEdgeAppearance = appearance
-//        }
     }
 }
 
