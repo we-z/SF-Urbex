298c78c6bb8138a7134c2866806f7977859d3297
we-z <yousefsalem00@gmail.com>
2024-12-28 01:59:49 -0800

    new icon

--- SF Urbex/TempView.swift
+++ SF Urbex/TempView.swift
@@ -11,38 +11,19 @@ import AVKit
 struct TempView: View {
     var body: some View {
         ZStack {
-            Color.white
-            Color.black
+            Color.blue
                 .aspectRatio(contentMode: .fit)
-            HStack(spacing: 42){
-                Divider()
-                    .frame(width: 6, height: 39)
-                    .overlay(.white)
-                    .rotationEffect(.degrees(69))
-                    .offset(x: 84, y: -30)
-                Divider()
-                    .frame(width: 6, height: 270)
-                    .overlay(.white)
-                    .offset(x: 20, y: 110)
-                Divider()
-                    .frame(width: 12, height: 390)
-                    .overlay(.white)
-                    .rotationEffect(.degrees(7))
-                Divider()
-                    .frame(width: 12, height: 390)
-                    .overlay(.white)
-                    .rotationEffect(.degrees(-7))
-                Divider()
-                    .frame(width: 6, height: 270)
-                    .overlay(.white)
-                    .offset(x: -20, y: 110)
-                Divider()
-                    .frame(width: 6, height: 39)
-                    .overlay(.white)
-                    .rotationEffect(.degrees(-69))
-                    .offset(x: -84, y: -30)
-            }
-            .offset(y: 25)
+            Image(systemName: "shield")
+                .foregroundColor(.white)
+                .font(.system(size: 330))
+            Image(systemName: "lock.fill")
+                .foregroundColor(.white)
+                .font(.system(size: 210))
+                .offset(y: -15)
+            Image(systemName: "camera.fill")
+                .foregroundColor(.blue)
+                .font(.system(size: 75))
+                .offset(y: 27)
             
         }
     }
