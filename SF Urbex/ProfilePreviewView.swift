5f3179ff7c3b031895d8a73bfe5ef2371c96b7ac
we-z <yousefsalem00@gmail.com>
2025-01-02 00:41:11 -0800

    continue from profile

--- SF Urbex/ProfilePreviewView.swift
+++ SF Urbex/ProfilePreviewView.swift
@@ -10,21 +10,33 @@ import SwiftUI
 struct ProfilePreviewView: View {
     var body: some View {
         VStack{
-            Circle()
-                .frame(width: 90, height: 90)
-                .foregroundColor(.secondary.opacity(0.3))
-                .padding()
-            Text("Anon.Urbexer")
-                .font(.largeTitle)
-                .bold()
-                .padding()
-            Text("Exploring the urban world. Capturing the moments that matter most.")
-                .multilineTextAlignment(.center)
+            VStack{
+                Circle()
+                    .frame(width: 150, height: 150)
+                    .foregroundColor(.secondary.opacity(0.3))
+                    .padding()
+                Text("Anon.Urbexer")
+                    .font(.largeTitle)
+                    .bold()
+                    .padding()
+                Text("Exploring the urban world. Capturing the moments that matter most.")
+                    .multilineTextAlignment(.center)
+            }
+            .padding()
+            .padding()
             Spacer()
+            Text("Continue")
+                .bold()
+                .font(.title3)
+                .padding()
+                .frame(maxWidth: .infinity)
+                .foregroundColor(.white)
+                .background(Color.blue)
+                .cornerRadius(15)
+                .padding()
         }
         .navigationTitle("Profile")
-        .padding()
-        .padding()
+        
     }
 }
 
