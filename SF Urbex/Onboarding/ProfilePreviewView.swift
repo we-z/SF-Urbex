792d975b7a16b066706d06873bd92706accc35e9
we-z <yousefsalem00@gmail.com>
2025-01-02 01:20:59 -0800

    add posts to profile preview

--- SF Urbex/Onboarding/ProfilePreviewView.swift
+++ SF Urbex/Onboarding/ProfilePreviewView.swift
@@ -8,22 +8,37 @@
 import SwiftUI
 
 struct ProfilePreviewView: View {
+    @StateObject private var cloudKitManager = CloudKitManager()
     var body: some View {
         VStack{
-            VStack{
-                Circle()
-                    .frame(width: 150, height: 150)
-                    .foregroundColor(.secondary.opacity(0.3))
-                    .padding()
-                Text("Anon.Urbexer")
-                    .font(.largeTitle)
-                    .bold()
-                    .padding()
-                Text("Exploring the urban world. Capturing the moments that matter most.")
-                    .multilineTextAlignment(.center)
+            ScrollView {
+                VStack{
+                    Circle()
+                        .frame(width: 150, height: 150)
+                        .foregroundColor(.secondary.opacity(0.3))
+                        .padding()
+                    Text("Anon.Urbexer")
+                        .font(.largeTitle)
+                        .bold()
+                        .padding()
+                    Text("Exploring the urban world. Capturing the moments that matter most.")
+                        .multilineTextAlignment(.center)
+                }
+                .padding()
+                .padding()
+                LazyVStack {
+                    ForEach(cloudKitManager.mediaItems) { item in
+                        MyPost(item: item)
+                            .padding(.vertical)
+                    }
+                }
             }
-            .padding()
-            .padding()
+            .refreshable {
+                cloudKitManager.fetchAllMedia()
+            }
+            .onAppear {
+                cloudKitManager.fetchAllMedia()
+            }
             Spacer()
             Text("Continue")
                 .bold()
