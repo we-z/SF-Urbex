9cfa4b3ceb8b386753e7489d2c629bd117637c40
we-z <yousefsalem00@gmail.com>
2025-01-01 20:17:38 -0800

    slogan

--- SF Urbex/PaywallView.swift
+++ SF Urbex/PaywallView.swift
@@ -11,40 +11,44 @@ struct PaywallView: View {
     @State var done: Bool = false
     var body: some View {
         VStack {
-            Spacer()
-            VStack {
-                // Large SF Symbol icon
-                ZStack{
-                    Image(systemName: "lock.shield.fill")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 150, height: 150)
-                        .foregroundColor(.white)
+            ScrollView{
+                VStack {
+                    // Large SF Symbol icon
+                    ZStack{
+                        Image(systemName: "lock.shield.fill")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 150, height: 150)
+                            .foregroundColor(.white)
+                            .padding()
+                        Image(systemName: "camera.fill")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 36, height: 36)
+                            .offset(y: 12)
+                            .foregroundColor(.white)
+                            .padding()
+                    }
+                    
+                    // Title
+                    Text("A small price to pay for privacy")
+                        .font(.largeTitle)
+                        .bold()
+                        .multilineTextAlignment(.center)
                         .padding()
-                    Image(systemName: "camera.fill")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 36, height: 36)
-                        .offset(y: 12)
-                        .foregroundColor(.white)
+                    
+                    // Description
+                    Text("Join the private photo sharing network. Explore and share photos privately fully E2E encrypted!")
+                        .font(.body)
+                        .multilineTextAlignment(.center)
                         .padding()
                 }
-                
-                // Title
-                Text("A small price to pay for privacy")
-                    .font(.largeTitle)
-                    .bold()
-                    .multilineTextAlignment(.center)
-                    .padding()
-                
-                // Description
-                Text("Join the private photo sharing network. Explore and share photos privately fully E2E encrypted!")
-                    .font(.body)
-                    .multilineTextAlignment(.center)
-                    .padding()
             }
-            Spacer()
-            // MARK: - Next / Rate Us Button
+            Text("14.99 / month")
+                .font(.title)
+                .padding()
+                .foregroundColor(.white)
+                .cornerRadius(21)
             Button{
                 withAnimation(.easeInOut) {
                     done = true
@@ -57,7 +61,7 @@ struct PaywallView: View {
                     .frame(maxWidth: .infinity)
                     .foregroundColor(.blue)
                     .background(Color.white)
-                    .cornerRadius(21)
+                    .cornerRadius(60)
                     .padding()
             }
         }
