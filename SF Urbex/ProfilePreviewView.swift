58c59a4270be5ab4e937e802684171ff359662fe
we-z <yousefsalem00@gmail.com>
2025-01-02 00:09:38 -0800

    ProfilePreviewView

---
+++ SF Urbex/ProfilePreviewView.swift
@@ -0,0 +1,32 @@
+//
+//  ProfilePreviewView.swift
+//  Hushpost
+//
+//  Created by Wheezy Capowdis on 1/2/25.
+//
+
+import SwiftUI
+
+struct ProfilePreviewView: View {
+    var body: some View {
+        VStack{
+            Circle()
+                .frame(width: 90, height: 90)
+                .foregroundColor(.secondary.opacity(0.3))
+                .padding()
+            Text("Anon.Urbexer")
+                .font(.largeTitle)
+                .bold()
+                .padding()
+            Text("Exploring the urban world. Capturing the moments that matter most.")
+                .multilineTextAlignment(.center)
+            Spacer()
+        }
+        .padding()
+        .padding()
+    }
+}
+
+#Preview {
+    ProfilePreviewView()
+}
