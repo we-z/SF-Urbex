4eb6abcd029523a3be6c339b374bad9ad6ed4ebe
we-z <yousefsalem00@gmail.com>
2025-01-01 18:44:18 -0800

    SetProfilePicView

--- SF Urbex/SetProfilePicView.swift
+++ SF Urbex/SetProfilePicView.swift
@@ -9,7 +9,45 @@ import SwiftUI
 
 struct SetProfilePicView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Text("Set Profile Picture")
+                .bold()
+                .font(.largeTitle)
+                .padding()
+            Text("You can always change your picture later or press Next to skip.")
+                .multilineTextAlignment(.center)
+                .padding()
+            Button {
+                
+            } label: {
+                ZStack{
+                    Circle()
+                        .frame(maxWidth: .infinity)
+                        .foregroundColor(.secondary.opacity(0.3))
+                        .padding()
+                        .padding()
+                    VStack {
+                        Image(systemName: "plus")
+                            .font(.largeTitle)
+                            .padding()
+                        Text("Select Photo")
+                            .bold()
+                            .font(.title2)
+                    }
+                }
+            }
+            Text("Next")
+                .bold()
+                .font(.title3)
+                .padding()
+                .frame(maxWidth: .infinity)
+                .foregroundColor(.white)
+                .background(Color.blue)
+                .cornerRadius(15)
+                .padding()
+            Spacer()
+        }
+        .background(Color.primary.colorInvert().ignoresSafeArea())
     }
 }
 
